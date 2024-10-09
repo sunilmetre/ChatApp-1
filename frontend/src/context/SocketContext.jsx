3b0c1ac48e8cbf76043773a56402f7eadc0de94c
sunilmetre777 <sunilmetre04@gmail.com>
2024-10-09 17:13:26 +0530

    url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chatapp-1-cn96.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
